e1012239abb6d154ce960b3e5d7e40ae87d2c3ad
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-08 03:11:13 +0530

    Added support for tuples and exception when nothing is passed

--- dfalchemy/converters.py
+++ dfalchemy/converters.py
@@ -2,11 +2,14 @@ import pandas as pd
 import json
 
 def to_dataframe(data):
+    if not data:
+        raise Exception("Please insert a List, Tuple, Dictionary or Json to convert to Dataframe")
+
     if isinstance(data, pd.DataFrame):
         return data
     elif isinstance(data, dict):
         return pd.DataFrame([data]) if not all(isinstance(v, list) for v in data.values()) else pd.DataFrame(data)
-    elif isinstance(data, list):
+    elif isinstance(data, list) or isinstance(data, tuple):
         return pd.DataFrame(data)
     elif isinstance(data, str):
         try:
