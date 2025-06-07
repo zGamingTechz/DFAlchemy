ea8fbc346347d82cbf699a0a7a1c2084103e5534
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-08 03:43:54 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dfalchemy',
+    version='1.0',
+    description='Generate, convert and manage DataFrames easily — perfect for devs and teachers.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Bhavya Soni',
+    url='https://github.com/zGamingTechz/DFAlchemy',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'faker'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
