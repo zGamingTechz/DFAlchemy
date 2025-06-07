02e5bc6a033229fbf3930fbc618c6327bc67f70d
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-08 03:05:31 +0530

    Added employees df generator

--- dfalchemy/generators.py
+++ dfalchemy/generators.py
@@ -4,7 +4,11 @@ import random
 
 faker = Faker()
 
-def generate_students_dataframe(count=10):
+
+def generate_students_dataframe(count):
+    if not count:
+        raise Exception("Please provide the number of rows")
+
     data = []
 
     for _ in range(count):
@@ -16,5 +20,24 @@ def generate_students_dataframe(count=10):
             'Attendance (%)': round(random.uniform(60, 100), 2)
         }
         data.append(student)
+
+    return pd.DataFrame(data)
+
+
+def generate_employees_dataframe(count):
+    if not count:
+        raise Exception("Please provide the number of rows")
+
+    data = []
+
+    for _ in range(count):
+        employee = {
+            'Name': faker.name(),
+            'Email': faker.email(),
+            'Age': random.randint(17, 25),
+            'Department': random.choice(['HR', 'Customer', 'IT', 'Product', 'Sales']),
+            'Salary': round(random.randint(1000, 10000))
+        }
+        data.append(employee)
 
     return pd.DataFrame(data)
