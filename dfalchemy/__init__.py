eb404725383d0abe9e84bdb7e6936db2164bf063
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-08 03:41:48 +0530

    Updated __init__

--- dfalchemy/__init__.py
+++ dfalchemy/__init__.py
@@ -1,10 +1,13 @@
-from .generators import generate_students_dataframe, generate_employees_dataframe
+from .generators import generate_students_dataframe, generate_employees_dataframe, generate_fruits_dataframe, generate_marks_dataframe, generate_business_dataframe
 from .converters import to_dataframe
 from .utils import save_df, info, drop_empty_cols
 
 __all__ = [
     'generate_students_dataframe',
     'generate_employees_dataframe',
+    'generate_fruits_dataframe',
+    'generate_marks_dataframe',
+    'generate_business_dataframe',
     'to_dataframe',
     'save_df',
     'info',
