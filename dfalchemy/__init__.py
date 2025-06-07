df2bd8e31c54d298831eaf6bd69d4433605e5ab9
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-08 03:14:06 +0530

    Added _all__ to init

--- dfalchemy/__init__.py
+++ dfalchemy/__init__.py
@@ -1,2 +1,4 @@
 from .generators import generate_students_dataframe, generate_employees_dataframe
 from .converters import to_dataframe
+
+__all__ = ['generate_students_dataframe', 'generate_employees_dataframe', 'to_dataframe']
